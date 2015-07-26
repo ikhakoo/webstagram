e4bb933bfa48d318993cda798e76838204e2727a
ikhakoo <ikhakoo@gmail.com>
2015-07-26 02:53:35 -0400

    created controller action and view for home page

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -1,1 +1,12 @@
-home.js
+angular.module('Webstagram')
+	.controller('HomeCtrl', function($scope) {
+
+	  $scope.isAuthenticated = function() {
+	    // check if logged in
+	  };
+
+	  $scope.linkInstagram = function() {
+	    // connect email account with instagram
+	  };
+
+	});
