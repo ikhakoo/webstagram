fbf5c6c089e8f59348d590a26718cad43d64ec31
ikhakoo <ikhakoo@gmail.com>
2015-07-26 03:15:19 -0400

    creating log in and home page

--- client/app.js
+++ client/app.js
@@ -1,21 +1,34 @@
-angular.module('Webstagram', ['ngRoute', 'ngMessages'])
-  .config(function($routeProvider) {
-  	$routeProvider
-		  .when('/', {
-		    templateUrl: 'views/home.html',
-		    controller: 'HomeCtrl'
-		  })
-		  .when('/login', {
-		    templateUrl: 'views/login.html',
-		    controller: 'LoginCtrl'
-		  })
-		  .when('/signup', {
-		    templateUrl: 'views/signup.html',
-		    controller: 'SignupCtrl'
-		  })
-		  .when('/photo/:id', {
-		    templateUrl: 'views/detail.html',
-		    controller: 'DetailCtrl'
-		  })
-		  .otherwise('/');
+angular.module('Webstagram', ['ngRoute', 'ngMessages', 'satellizer'])
+  .config(function($routeProvider, $authProvider) {
+	  	$routeProvider
+			  .when('/', {
+			    templateUrl: 'views/home.html',
+			    controller: 'HomeCtrl'
+			  })
+			  .when('/login', {
+			    templateUrl: 'views/login.html',
+			    controller: 'LoginCtrl'
+			  })
+			  .when('/signup', {
+			    templateUrl: 'views/signup.html',
+			    controller: 'SignupCtrl'
+			  })
+			  .when('/photo/:id', {
+			    templateUrl: 'views/detail.html',
+			    controller: 'DetailCtrl'
+			  })
+			  .otherwise('/');
+
+		  $authProvider.loginUrl = 'http://localhost:3000/auth/login';
+			$authProvider.signupUrl = 'http://localhost:3000/auth/signup';
+			$authProvider.oauth2({
+			  name: 'instagram',
+			  url: 'http://localhost:3000/auth/instagram',
+			  redirectUri: 'http://localhost:8000',
+			  clientId: '38da2591128d48f297a2ace1aa0514e6',
+			  requiredUrlParams: ['scope'],
+			  scope: ['likes'],
+			  scopeDelimiter: '+',
+			  authorizationEndpoint: 'https://api.instagram.com/oauth/authorize'
+			});
   });
---
+++ client/server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	db: 'localhost',
+	clientSecret: process.env.clientSecret || "840eeca7427046dab143bb39dc92f12e",
+	tokenSecret: process.env.tokenSecret || 'pick a hard string to guess'
+};
